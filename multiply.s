582601edfeec3e0e59146ec55c520a8775169284
Maxwell <maxwang@udel.edu>
2022-04-14 09:39:15 -0400

    The loop is finished, will need to test

--- multiply.s
+++ multiply.s
@@ -3,12 +3,13 @@
 _start:
 	MOV r0, #1
 	MOV r1, r0
-LOOP: ADD r0,r0, r1
+LOOP: 
       CMP r0, #0
       B LOOP
-
+      ADD r1, r1, r0
+      ADD R0, R0, #1		
 
 EXIT: MOV r0,#0
-	MOV r8,#93
-	SVC #0
+      MOV r8,#93
+      SVC #0
 
