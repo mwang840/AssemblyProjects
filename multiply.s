121adc476b4f7f476b36886bf748620dfb314811
Maxwell <maxwang@udel.edu>
2022-04-14 20:30:29 -0400

    Made a while loop and cleaned up corruption in multiply.o file

--- multiply.s
+++ multiply.s
@@ -1,14 +1,14 @@
 .text
 .global _start
 _start:
-	MOV r0, #1
-	MOV r1, r0
+	MOV r0, #5
+	MOV r1, #3
 LOOP: 
-      CMP r0, #0
+      CMP r0, r1
+      ADD r0, r0, r0
+      SUB R1, R1, #1
+      BEQ EXIT			
       B LOOP
-      ADD r1, r1, r0
-      ADD R0, R0, #1		
-      BEQ EXIT
 	
 EXIT: MOV r0,#0
       MOV r8,#93
