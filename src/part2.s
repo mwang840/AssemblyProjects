429599942baea787483071421741d21d6cff6b15
Maxwell Wang <maxwang@udel.edu>
2022-05-07 08:43:36 -0400

    Started part 2 of this file

---
+++ src/part2.s
@@ -0,0 +1,32 @@
+.data
+	FILENAME: .asciz "/dev/fb1"
+	FD: .word 0
+	FB: .word 0
+	
+.global openfb
+.global closefb
+.global getColor
+.global setPixel
+.text
+_start:
+        #open frame buffer 1
+        mov r0,#1
+        bl openfb
+        cmp r0,#0
+        blt _start_exit
+
+        #initializes outer loop
+  LOOP: mov r1 #0
+        cmp #8
+        ADD r1 #1
+  
+        #initializes inner loop
+  LOOP: mov r2 #0
+        cmp #8
+        mov r1 r1
+        mov r2 r2
+        b1 setPixel
+        ADD r2 #1
+
+        b1 closefb
+
