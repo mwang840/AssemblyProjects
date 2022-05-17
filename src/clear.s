3fa06796147ee2e456f75c8ce948fe60faace5a6
Maxwell Wang <maxwang@udel.edu>
2022-05-17 17:06:00 -0400

    Now added a reminder to set the color to blank

--- src/clear.s
+++ src/clear.s
@@ -12,18 +12,32 @@
 _start:
         #open frame buffer 1
         mov r0,#1
-        mov r1, #0
-        mov r2, #0
         bl openfb
         cmp r0,#0
         blt _start_exit
 
+        #Sets color to blank then get the color securing the blank
+        mov r8,r0
+        mov r0, #0
+        mov r1, #0
+        mov r2, #0
+        bl getColor
+        mov r6, #0
+
+        #sets up the variables for the loop
+        mov r0, #0
+        mov r1, #0
+        mov r2, #0
+        mov r9, #0
+        mov r10, #0
+        b INNERLOOP
         #initializes outer loop
-  OUTERLOOP: ADD r1, #1
+  OUTERLOOP: 
+        ADD r1, #1
         cmp r1, #8
   
         #initializes inner loop
-  INNERLOOP: mov r2, #0
+  INNERLOOP:
         cmp r2, #8
         bl setPixel
         ADD r2, #1
