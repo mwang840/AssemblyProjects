d3780bba8775934cdc50a9fc2eee476a650a5c7d
Maxwell Wang <maxwang@udel.edu>
2022-05-17 17:59:09 -0400

    cleared up headers

--- src/part3.s
+++ src/part3.s
@@ -1,3 +1,4 @@
+.global _start
 .data
 	FILENAME: .asciz "/dev/fb1"
 	FD: .word 0
@@ -8,22 +9,19 @@
 .global getColor
 .global setPixel
 .text
-
-#Opens Frame buffer 1
 _start:
 	mov r0,#1
-	mov r3, #0
-	mov r4, #0
 	bl openfb
 	cmp r0,#0
 	blt _start_exit
 #Gets the color red and store it in r6
-	mov r8,r0
 	mov r0, #31
 	mov r1, #0
 	mov r2, #0
 	bl getColor
 	mov r6, r0
+	mov r10, #8
+	mov r11, #8
 
 #Loops and puts color red to make the x on the screen
 	LOOP: ADD r3, #1
